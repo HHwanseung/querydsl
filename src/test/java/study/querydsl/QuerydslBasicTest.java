81b9a7a4dc2558585fde84a462caf03280cc6132
seunghwanLee <dkqlch@naver.com>
2022-06-30 23:49:25 +0900

    정렬

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -125,7 +125,7 @@ public class QuerydslBasicTest {
 
         List<Member> result = queryFactory
                 .selectFrom(member)
-                .where(member.age.eq(400))
+                .where(member.age.eq(100))
                 .orderBy(member.age.desc(), member.username.asc().nullsLast())
                 .fetch();
 
