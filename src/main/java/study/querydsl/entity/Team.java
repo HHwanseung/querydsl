c86b179af1a95fb52a3ca08b6180a9ef82c647de
seunghwanLee <dkqlch@naver.com>
2022-07-02 23:25:57 +0900

    조인 - 기본 조인

--- src/main/java/study/querydsl/entity/Team.java
+++ src/main/java/study/querydsl/entity/Team.java
@@ -18,7 +18,6 @@ public class Team {
     @Id @GeneratedValue
     private Long id;
     private String name;
-    private String Department;
 
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
