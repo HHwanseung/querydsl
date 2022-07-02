26720f0dd2e512ca973c07bc149678ddf0b217ee
seunghwanLee <dkqlch@naver.com>
2022-07-02 23:27:06 +0900

    조인 - 기본 조인

--- src/main/java/study/querydsl/entity/Team.java
+++ src/main/java/study/querydsl/entity/Team.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.xml.stream.Location;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +17,7 @@ public class Team {
 
     @Id @GeneratedValue
     private Long id;
-    private String location;
+    private String name;
 
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
