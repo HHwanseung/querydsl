7bdf97083feeacf8e9afd5a584eb5edbbb9dfec9
seunghwanLee <dkqlch@naver.com>
2022-07-13 18:26:05 +0900

    Querydsl 지원 클래스 직접 만들기

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -10,7 +10,6 @@ public class MemberDto {
 
     private String username;
     private int age;
-    private boolean obstacle;
 
     @QueryProjection
     public MemberDto(String username, int age) {
