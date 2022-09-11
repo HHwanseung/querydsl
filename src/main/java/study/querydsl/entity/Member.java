7c809e4b116889d59f251257b61a9cbae164c6f5
seunghwanLee <dkqlch@naver.com>
2022-09-11 22:58:02 +0900

    Querydsl 지원 클래스 직접 만들기

--- src/main/java/study/querydsl/entity/Member.java
+++ src/main/java/study/querydsl/entity/Member.java
@@ -15,6 +15,7 @@ public class Member {
     private Long id;
     private String username;
     private int age;
+    private String alone;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
